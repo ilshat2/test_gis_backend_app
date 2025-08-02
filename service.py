9c08d9b84341934868c19c65c84266b6c574f326
ilshat kushmanbetov <ilshat26101993@list.ru>
2025-08-02 20:02:19 +0300

    fix: В ответ на post запрос приходит круглый полигон

--- service.py
+++ service.py
@@ -1,7 +1,10 @@
 import math
+import pyproj
 from google_sheets_access import append_row
 from models import Location
 from decimal import Decimal
+from shapely.geometry import Point, mapping
+from shapely.ops import transform
 
 
 class LocationService:
@@ -19,19 +22,29 @@ class LocationService:
         await append_row(data.to_row(str(coverage_area)))
         return coverage_area
 
-    def to_geojson(self, data: Location, coverage_area):
+    def to_geojson(self, data: Location, coverage_area, num_points=64):
         """
-        Возвращает geojson объект со всеми свойствами.
+        Возвращает geojson объект с полигоном вокруг точки.
         """
+        proj_wgs84 = pyproj.CRS("EPSG:4326")
+        proj_merc = pyproj.CRS("EPSG:3857")
+        project_to_m = pyproj.Transformer.from_crs(
+            proj_wgs84, proj_merc, always_xy=True
+        ).transform
+        project_to_deg = pyproj.Transformer.from_crs(
+            proj_merc, proj_wgs84, always_xy=True
+        ).transform
+
+        centre_point = transform(project_to_m, Point(float(data.lon), float(data.lat)))
+        circle = centre_point.buffer(
+            float(data.radius),
+            resolution=num_points,
+        )
+        circle_wgs84 = transform(project_to_deg, circle)
+
         return {
             "type": "Feature",
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    float(data.lon),
-                    float(data.lat),
-                ],  # важен порядок [lon, lan]
-            },
+            "geometry": mapping(circle_wgs84),
             "properties": {
                 "id": data.id,
                 "date": data.date,
